cd8602f74c97aca91ef2954da63563fda12e5031
Marcelo Silva <ribmarc@gmail.com>
2022-04-17 11:20:51 -0300

    chore(accounts): add .env in typeorm

--- account/src/infra/typeorm/index.ts
+++ account/src/infra/typeorm/index.ts
@@ -1,3 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any  */
 import { createConnection } from 'typeorm';
 
-createConnection();
+const config: any = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  migrationsTableName: 'migrations',
+  entities: ['./src/infra/typeorm/entities/*.ts'],
+  migrations: ['./src/infra/typeorm/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/infra/typeorm/migrations'
+  }
+};
+
+createConnection(config);
