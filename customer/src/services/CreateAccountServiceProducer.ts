9375e33ec003ebb6ca2a1236c5ad42c804e84986
Marcelo Silva <ribmarc@gmail.com>
2022-04-17 11:28:03 -0300

    refactor(account): separates host and port kafka

--- customer/src/services/CreateAccountServiceProducer.ts
+++ customer/src/services/CreateAccountServiceProducer.ts
@@ -4,7 +4,9 @@ import { Kafka } from 'kafkajs';
 export default class CreateAccountServiceProducer {
   static async execute(cpf: string): Promise<void> {
     const clientId = process.env.CLIENT_ID_CREATE_ACCOUNT_KAFKA || '';
-    const brokers = [process.env.BROKERS_KAFKA || ''];
+    const brokers = [
+      `${process.env.KAFKA_HOST}:${process.env.KAFKA_PORT}` || ''
+    ];
     const topic = process.env.TOPIC_CREATE_ACCOUNT_KAFKA || '';
 
     const kafka = new Kafka({ clientId, brokers });
